3a11c1e3cac65dd95ba96908063f07b4adf352be
Romain SALVAN <romain.salvan@g-mail.fr>
2021-03-08 16:21:48 +0100

    Html s2

---
+++ SEMESTER 2/TP1/contenu.js
@@ -0,0 +1,25 @@
+{
+     "prenom": "Jean",
+     "nom": "Martin",
+     "adresse":
+     {
+         "rue": "10, rue des Vignes",
+         "ville": "Albi",
+         "cp": "81000"
+     },
+     "contacts":
+     [
+		 {
+           "type": "domicile",
+           "numero": "05 63 12 34 56"
+         },
+         {
+           "type": "mobile",
+           "numero": "06 12 34 56 78"
+         },
+		 {
+           "type": "bureau",
+           "numero": "05 61 23 45 67"
+         }
+     ]
+ }
